d84b3104e999acc3bd693ec90135471855d2259c
Masoodkhan501 <141975307+Masoodkhan501@users.noreply.github.com>
2026-05-31 09:57:14 +0530

    provided getters for HttpReqWithoutBuilder class to each field

--- src/com/masood/HttpRequestObject/HttpReqWithoutBuilder.java
+++ src/com/masood/HttpRequestObject/HttpReqWithoutBuilder.java
@@ -9,33 +9,35 @@ public class HttpReqWithoutBuilder {
 	private Map<String, String> queryParam;
 	private String body;
 	private Integer timeOut;
-	
-	// 								1. Telescoping problem
+
+	// 1. Telescoping problem
 	/*
-	 * In this section u see we are building the constructors for each field or combo of it.
+	 * In this section u see we are building the constructors for each field or
+	 * combo of it.
 	 */
-	
+
 	// Constructor For single Field
-	public HttpReqWithoutBuilder(String url){
+	public HttpReqWithoutBuilder(String url) {
 		this.url = url;
 	}
-	
+
 	// Constructor for accepting two fields (url, method)
 	public HttpReqWithoutBuilder(String url, String method) {
 		this.url = url;
 		this.method = method;
 	}
-	
-	//Constructor For accepting three fields
+
+	// Constructor For accepting three fields
 	public HttpReqWithoutBuilder(String url, String method, Integer timeOut) {
 		this.url = url;
 		this.method = method;
 		this.timeOut = timeOut;
 	}
-	
-	//Constructor for accepting all the fields
+
+	// Constructor for accepting all the fields
 	public HttpReqWithoutBuilder(String url, String method, Map<String, String> header, 
-			Map<String, String> queryParam, String body, Integer timeOut) {
+			Map<String, String> queryParam,
+			String body, Integer timeOut) {
 		this.url = url;
 		this.method = method;
 		this.header = header;
@@ -44,49 +46,75 @@ public class HttpReqWithoutBuilder {
 		this.timeOut = timeOut;
 	}
 
-	
-	// 									2. This Shows MUTABLE problem
-	
+	// 2. This Shows MUTABLE problem
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	
+
 	public void setMethod(String method) {
 		this.method = method;
 	}
-	
+
 	public void setHeader(Map<String, String> header) {
 		this.header = header;
 	}
-	
+
 	public void setQueryParam(Map<String, String> queryParam) {
 		this.queryParam = queryParam;
 	}
-	
+
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
+
 	public void setTimeOut(Integer timeOut) {
 		this.timeOut = timeOut;
 	}
-	
-	//	4. Scattered validation(here u may not see but in real time for checking each field
-	//     may need different if block with different checkers
-	
-	
+
+	// 4. Scattered validation(here u may not see but in real time for checking each
+	// field
+	// may need different if block with different checkers
+
 	// Source for 3 problem that is inconsistent error
-	public void execute(String url, String method, Map<String, String> header, 
-			Map<String, String> queryParam, String body, Integer timeOut) {
+	public void execute(String url, String method, Map<String, String> header,
+			Map<String, String> queryParam,
+			String body, Integer timeOut) {
 		boolean validationfailed = false;
-		if(url == null || method == null || 
-				header == null || queryParam == null || body == null 
-				|| timeOut == null) validationfailed = true;
-		
-		if(validationfailed) System.out.println("Some fields are missing please check before"
-				+ "executing.");
-		else System.out.println("Everything is fine u can execute");
-		
+		if (url == null || method == null || 
+				header == null || queryParam == null || 
+				body == null || timeOut == null)
+			validationfailed = true;
+
+		if (validationfailed)
+			System.out.println("Some fields are missing please check before" + "executing.");
+		else
+			System.out.println("Everything is fine u can execute");
+
 	}
-	
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public Map<String, String> getHeader() {
+		return header;
+	}
+
+	public Map<String, String> getQueryParam() {
+		return queryParam;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public Integer getTimeOut() {
+		return timeOut;
+	}
+
 }
