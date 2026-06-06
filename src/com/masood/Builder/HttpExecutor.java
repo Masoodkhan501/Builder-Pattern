8badc24ad5c692bd8f785a515f34dda0f6614aea
Masoodkhan501 <141975307+Masoodkhan501@users.noreply.github.com>
2026-06-06 21:51:42 +0530

    set up is done need some more optimization and also changes to make it future proof

--- src/com/masood/Builder/HttpExecutor.java
+++ src/com/masood/Builder/HttpExecutor.java
@@ -1,5 +1,7 @@
 package com.masood.Builder;
 
+import java.util.List;
+
 public class HttpExecutor {
 	
 	private HttpReqWithBuilder req;
@@ -8,8 +10,16 @@ public class HttpExecutor {
 		this.req = req;
 	}
 	
-//	public String execute() {
-//		List<String> errors = 
-//	}
+	public String execute() {
+		List<String> errors = ValidateHttp.validate(req);
+		String errorResult = "";
+		if(errors.isEmpty()) return "everything is good. Executed perfectly";
+		else {
+			for(String error:errors) {
+				errorResult += error;
+			}
+			return errorResult;
+		}
+	}
 
 }
